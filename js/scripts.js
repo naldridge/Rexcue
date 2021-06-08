e2e840755654265099f7615dc6f977889bb06541
Nick Aldridge <naldridge1360@gmail.com>
2021-06-08 09:38:11 -0500

    nothing changed from original

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 `use strict`
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch()
+        .then(function () {
+            return response.json();
+        })
+        .then(function (data) {
+            return data;
+        })
+        .catch(function (error) {
+            console.error("ERROR: ", error);
+            return error;
+        })
+        
+});
+
+
