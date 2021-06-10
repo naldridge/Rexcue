1d5e6dcf9162192230c0ae2f4696a4dd243ba5c1
Zach Gleeson <zachary.a.gleeson@gmail.com>
2021-06-09 21:12:49 -0500

    Started modal

--- js/scripts.js
+++ js/scripts.js
@@ -17,19 +17,19 @@ document.addEventListener('DOMContentLoaded', function () {
 }); */
 
 //Style the Personality Quiz input selections
-const inputs = document.querySelectorAll('input');
-const showMeButton = document.getElementById('showMe');
+// const inputs = document.querySelectorAll('input');
+// const showMeButton = document.getElementById('showMe');
 
-showMeButton.addEventListener('click', function() {
-    inputs.forEach(function(input) {
-        if (input.checked) {
-            input.parentNode.style.color = "green";
-        } else {
-            console.log("THIS IS NOT CHECKED")
-            input.parentNode.style.color = "red";
-        }
-    });
-});
+// showMeButton.addEventListener('click', function() {
+//     inputs.forEach(function(input) {
+//         if (input.checked) {
+//             input.parentNode.style.color = "green";
+//         } else {
+//             console.log("THIS IS NOT CHECKED")
+//             input.parentNode.style.color = "red";
+//         }
+//     });
+// });
 
 // Get the Sidebar
 const mySidebar = document.getElementById("mySidebar");
@@ -53,3 +53,54 @@ function w3_close() {
     mySidebar.style.display = "none";
     overlayBg.style.display = "none";
 }
+
+// Joke Modal ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function toggleModal() {
+    const overlay = document.querySelector('#overlay');
+    overlay.classList.toggle('visible');
+}
+
+const closeModalButton = document.querySelector('#closeModal');
+
+closeModalButton.addEventListener('click', function() {
+    toggleModal();
+});
+
+
+function buildQuote(theQuote) {
+    // 1. Select the #modal element
+    // 2. Select the paragraph element from the #modal
+    // 3. Change the innerText of the paragraph to be the quote
+    // 4. Profit
+
+    const modalElement = document.querySelector('#modal p');
+    modalElement.innerText = theQuote;
+    toggleModal();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://api.chucknorris.io/jokes/random?category=dev')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            buildQuote(data.value);
+        })
+        .catch(function(error) {
+            console.error("ERROR: ", error);
+            return error;
+        });
+
+    document.addEventListener('keydown', function(event) {
+        console.log("the key that was pressed is: ", event.key);
+        if (event.key === 'Escape') {
+            toggleModal();
+        }
+    });
+
+    const overlay = document.querySelector('#overlay');
+
+    overlay.addEventListener('click', function() {
+        toggleModal();
+    });
+});
