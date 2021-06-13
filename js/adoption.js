5d4cf891de81488606b3498be2992e389aa12eed
Nick Aldridge <naldridge1360@gmail.com>
2021-06-13 15:46:28 -0500

    created shelters page and linked on navbar for current pages

--- js/adoption.js
+++ js/adoption.js
@@ -1,11 +1,35 @@
 'use strict';
-
+const rescueSearch = document.getElementById('runSearch');
+const rescueResults = document.getElementById("rescueResults");
 
 document.addEventListener('DOMContentLoaded', function () {
+    
+/*  // gets geolocation data in the form of Lat/Longs
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            rescueResults.innerText = "Geolocation is not supported by this browser.";
+        }
+    };
+
+    function showPosition(position) {
+        console.log("Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude);
+    };
+    getLocation();
+    showPosition(); */
+});
+
+rescueSearch.addEventListener('click', function () {
+    const zipInput = document.getElementById('zipcodeTxt').value;
+
+    console.log(zipInput);
 
     //petfinder API fetch request
     //petfinder recommends cURL - which I've converted to a fetch request
     //function fetchPetfinder concats 2 fetch requests
+    //API STRUCTURE --> https://api.petfinder.com/v2/{CATEGORY}/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}
     function fetchPetfinder() {
         //original fetch simply gets an access token which is used to query API
         fetch("https://api.petfinder.com/v2/oauth2/token", {
@@ -14,30 +38,29 @@ document.addEventListener('DOMContentLoaded', function () {
                 "Content-Type": "application/x-www-form-urlencoded"
             },
             method: "POST"
-        })
-            .then(function (response) {
-                return response.json();
-            }).then(function (data) {
-                console.log('token ', data);
-                return fetch("https://api.petfinder.com/v2/types/dog/breeds", {
+        }).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            //console.log('token ', data);
+            return fetch(`https://api.petfinder.com/v2/animals?type=dog&location=${zipInput}&distance=10`, {
                 //second fetch request uses access token get API data
-                    headers: {
-                        Authorization: data.token_type + " " + data.access_token,
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }   
-                });
-            }).then(function (response) {
-                // Return the API response as JSON
-                return response.json();
-            
-            }).then(function (data) {
-                // Log the pet data
-                console.log(data);
-            
-            }).catch(function (error) {
-                console.error("ERROR: ", error);
-                return error;
+                headers: {
+                    Authorization: data.token_type + " " + data.access_token,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
             });
+        }).then(function (response) {
+            // Return the API response as JSON
+            return response.json();
+
+        }).then(function (data) {
+            // Log the pet data
+            console.log(data);
+
+        }).catch(function (error) {
+            console.error("ERROR: ", error);
+            return error;
+        });
     };
 
     fetchPetfinder();
