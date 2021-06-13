410a86f55a2e5a4f93f944bf8aa0c13eb8d476a5
Zach Gleeson <zachary.a.gleeson@gmail.com>
2021-06-12 20:30:00 -0500

    Made all pages responsive and updated all links, fixed modal to only work on modal page, updated index pics to be on cards

--- js/joke.js
+++ js/joke.js
@@ -1,4 +1,57 @@
-`use strict`
+// Joke Modal ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function toggleModal() {
+    const overlay = document.querySelector('#overlay');
+    overlay.classList.toggle('visible');
+}
+
+const closeModalButton = document.querySelector('#closeModal');
+
+closeModalButton.addEventListener('click', function() {
+    toggleModal();
+});
+
+
+function buildQuote(theQuote) {
+    // 1. Select the #modal element
+    // 2. Select the paragraph element from the #modal
+    // 3. Change the innerText of the paragraph to be the quote
+    // 4. Profit
+
+    const modalElement = document.querySelector('#modal p');
+    modalElement.innerText = theQuote;
+    toggleModal();
+}
+
+// const tellJoke = document.querySelector(`#dogJokeModal`);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://api.chucknorris.io/jokes/random?category=dev')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            buildQuote(data.value);
+        })
+        .catch(function(error) {
+            console.error("ERROR: ", error);
+            return error;
+        });
+
+    document.addEventListener('keydown', function(event) {
+        console.log("the key that was pressed is: ", event.key);
+        if (event.key === 'Escape') {
+            toggleModal();
+        }
+    });
+
+    const overlay = document.querySelector('#overlay');
+
+    overlay.addEventListener('click', function() {
+        toggleModal();
+    });
+});
+
 
 // Show Breed pictures ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function showBreeds(data) {
