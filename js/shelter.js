89aa4b5d2f3a0cd0372ed876521b5fe3cc40aa17
Zach Gleeson <zachary.a.gleeson@gmail.com>
2021-06-13 16:32:49 -0500

    Added shelters page and added link to all other pages

---
+++ js/shelter.js
@@ -0,0 +1,150 @@
+`use strict`
+
+// Show Breed pictures ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function showBreeds(data) {
+    console.log(data, 'inside show breeds')
+    const image = document.createElement('img')
+    image.src = data.message;
+    console.log(image)
+
+    const container = document.querySelector('#adoptDog1')
+    container.append(image)
+}
+
+function showBreeds2(data) {
+    console.log(data, 'inside show breeds')
+    const image = document.createElement('img')
+    image.src = data.message;
+    console.log(image)
+
+    const container = document.querySelector('#adoptDog2')
+    container.append(image)
+}
+
+function showBreeds3(data) {
+    console.log(data, 'inside show breeds')
+    const image = document.createElement('img')
+    image.src = data.message;
+    console.log(image)
+
+    const container = document.querySelector('#adoptDog3')
+    container.append(image)
+}
+
+function showBreeds4(data) {
+    console.log(data, 'inside show breeds')
+    const image = document.createElement('img')
+    image.src = data.message;
+    console.log(image)
+
+    const container = document.querySelector('#adoptDog4')
+    container.append(image)
+}
+
+function showBreeds5(data) {
+    console.log(data, 'inside show breeds')
+    const image = document.createElement('img')
+    image.src = data.message;
+    console.log(image)
+
+    const container = document.querySelector('#adoptDog5')
+    container.append(image)
+}
+
+function showBreeds6(data) {
+    console.log(data, 'inside show breeds')
+    const image = document.createElement('img')
+    image.src = data.message;
+    console.log(image)
+
+    const container = document.querySelector('#adoptDog6')
+    container.append(image)
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+
+            showBreeds(data);
+
+        })
+        .catch(function(error) {
+            console.error("ERROR: ", error);
+            return error;
+        });
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+
+            showBreeds2(data);
+
+        })
+        .catch(function(error) {
+            console.error("ERROR: ", error);
+            return error;
+        });
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+
+            showBreeds3(data);
+
+        })
+        .catch(function(error) {
+            console.error("ERROR: ", error);
+            return error;
+        });
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+
+            showBreeds4(data);
+
+        })
+        .catch(function(error) {
+            console.error("ERROR: ", error);
+            return error;
+        });
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+
+            showBreeds5(data);
+
+        })
+        .catch(function(error) {
+            console.error("ERROR: ", error);
+            return error;
+        });
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+
+            showBreeds6(data);
+
+        })
+        .catch(function(error) {
+            console.error("ERROR: ", error);
+            return error;
+        });
+});
